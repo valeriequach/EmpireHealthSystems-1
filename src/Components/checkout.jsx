7cf53ffe1bbbf65dd476eafdcb968c498356037a
David James Dimalanta <david.dimalanta@mail.utoronto.ca>
2023-09-26 11:24:17 -0400

    payment test

--- src/Components/checkout.jsx
+++ src/Components/checkout.jsx
@@ -5,11 +5,11 @@ const ProductDisplay = () => (
   <section>
     <div className="product">
       <img
-        src="https://i.imgur.com/EHyR2nP.png"
-        alt="The cover of Stubborn Attachments"
+        src=""
+        alt=""
       />
       <div className="description">
-      <h3>Stubborn Attachments</h3>
+      <h3>Service</h3>
       <h5>$20.00</h5>
       </div>
     </div>
